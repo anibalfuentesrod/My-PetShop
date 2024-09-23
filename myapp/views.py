9cddc3170718d02ab1522e7d1c909ab2c60004cf
Anibal <157289919+anibalfuentesrod@users.noreply.github.com>
2024-09-23 12:25:47 -0400

    pushing folders and all

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 # Create your views here.
+
+
+def hello_names(request):
+    return HttpResponse('hola anibal y kryss')
+
+
