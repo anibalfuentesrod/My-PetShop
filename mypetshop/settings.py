7298753420f036f257cde50b083aebe567ff89e6
Anibal <157289919+anibalfuentesrod@users.noreply.github.com>
2024-09-23 16:57:43 -0400

    pushing models, to databases

--- mypetshop/settings.py
+++ mypetshop/settings.py
@@ -11,7 +11,15 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from decouple import config
 
+
+# Stripe settings
+STRIPE_SECRET_KEY = config('STRIPE_SECRET_KEY')
+STRIPE_PUBLISHABLE_KEY = config('STRIPE_PUBLISHABLE_KEY')
+
+DEBUG = config('DEBUG', default=False, cast=bool)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -116,7 +124,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+# Esto es para media files, los files q el user va a poner las imagenes
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'myapp/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
