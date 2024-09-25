2625e778f85ece93412a79f0e3db30af4a094a8d
Anibal <157289919+anibalfuentesrod@users.noreply.github.com>
2024-09-25 09:26:25 -0400

    al momento estamos bien

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .views import CreateCheckoutSessionView
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -8,7 +9,8 @@ urlpatterns = [
     path('logout/', views.logout_form, name='logout'),
     path('profile/', views.user_profile, name='profile'),
     path('products/', views.products, name='products'),
-    path('order/<int:product_id>/', views.product_detail, name='product_detail'),
+    path('products/<int:product_id>/', views.product_detail, name='product_detail'),
     path('success/', views.success_view, name='success'),
-    path('cancel/', views.cancel_view, name='cancel')
+    path('cancel/', views.cancel_view, name='cancel'),
+    path('create-checkout-session/<int:pk>/', CreateCheckoutSessionView.as_view(), name='create-checkout-session'),
 ]
